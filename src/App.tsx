cac0f4c55cb9e6238703325c9388f442a4fb6a03
Davi Nakano <davinakanoca@gmail.com>
2023-08-31 14:41:09 -0700

    Create task Input + Button (styles only)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import styles from "./App.module.css";
+import CreateTaskButton from "./components/CreateTaskButton";
+
 import Header from "./components/Header";
+import Input from "./components/Input";
 
 function App() {
   return (
     <>
       <Header />
+      <main className={styles.main}>
+        <section className={styles.inputSection}>
+          <Input placeholder="Add a new task" />
+          <CreateTaskButton />
+        </section>
+        <section></section>
+      </main>
     </>
   );
 }
